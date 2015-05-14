30e08edb9d37a67fe7b4cbb4b0affcb2be0ce7ea
Rodja Trappe <mail@rodja.net>
2015-05-14 13:31:15 +0200

    init project

---
+++ Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PriorityTest.Console
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            new Test().Run();
+        }
+    }
+}
